1ff2d637d3db5bfa551e30d83ecb13e7b72a8730
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-02-21 22:06:22 +0530

    Added code for new pages and some design.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="product" element={<Product />} />
-        <Route path="/" element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
