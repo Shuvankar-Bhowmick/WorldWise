f6c32e96c2137d1591ba31a6008ca0ff6771fde6
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-02-22 13:37:24 +0530

    Added more components.

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import Logo from "../Logo/Logo";
+import styles from "./Sidebar.module.css";
+import AppNav from "../AppNav/AppNav";
+import Footer from "../Footer/Footer";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
