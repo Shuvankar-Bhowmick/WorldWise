1c847bef61ba0597a38662206e39a70cacf78940
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2024-02-23 21:15:14 +0530

    Added nested routes and index routes in App.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import Logo from "../Logo/Logo";
 import styles from "./Sidebar.module.css";
 import AppNav from "../AppNav/AppNav";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -9,7 +10,8 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
+
       <Footer />
     </div>
   );
